a4cdaaf30638d27518ee6da065b2fd4b245ead43
Michal Fabry <michal@fabry.sk>
2012-09-17 21:21:15 +0200

    add comments and key codes

--- chrome/global.js
+++ chrome/global.js
@@ -1,7 +1,22 @@
+/**
+ * Some key codes
+ */
+var KEY_TAB    = 9;
+var KEY_ENTER  = 13;
+var KEY_ESCAPE = 27;
+
+
+/**
+ * document.getElementById shortener
+ */
 var $ = function(aElementId) {
 	return document.getElementById(aElementId);
 };
 
+
+/**
+ * Some useful methods for html elements
+ */
 HTMLElement.prototype.toggle = function() {
 	if (this.style) {
 		if (this.style.display == 'block') {
